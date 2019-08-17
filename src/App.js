f46794f0b7efb5e53a2cd74e2998bf8a42870247
vitali-dev <vitali.repin@gmail.com>
2019-08-17 14:12:02 +0200

    Added Card Component

--- src/App.js
+++ src/App.js
@@ -18,12 +18,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList name='Vitali'>
-          {
-            this.state.monsters.map(monster => <h1> {monster.name} </h1>)
-          }
-        </CardList>
-
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
