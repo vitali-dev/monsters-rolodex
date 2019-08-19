18ec67073c926037222f75139b65d032a110ae1f
vitali-dev <vitali.repin@gmail.com>
2019-08-19 20:47:05 +0200

    Updates

---
+++ precache-manifest.9cdc96e40f8e0f0c5347aa3ddfb009e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44fced50ff6534c93a602ed7899f1d74",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b3eef32b3b2f5f272775",
+    "url": "/monsters-rolodex/static/css/main.a1aeaee7.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "b3eef32b3b2f5f272775",
+    "url": "/monsters-rolodex/static/js/main.c0e2a0c0.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
