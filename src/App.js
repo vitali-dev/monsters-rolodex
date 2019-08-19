c517ab895fbe21d085cf6c54adf7562308d77d3e
vitali-dev <vitali.repin@gmail.com>
2019-08-19 20:36:48 +0200

    Added Class method

--- src/App.js
+++ src/App.js
@@ -17,16 +17,22 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  };
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
     );
+
     return (
       <div className="App">
+        <h1> Monster Rolodex</h1>
         <SearchBox
           placeholder='search monsters'
-          handleChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
